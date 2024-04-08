e505ec1f29b807a6d8171815db4de8fa63ffd81a
SpaceProdigy <podlesnyoleg93@gmail.com>
2024-04-08 01:07:11 +0000

    Deploying to gh-pages from @ SpaceProdigy/Medicine_Delivery_app@a3245f07d85aa49f1e643d4270c6dc99918c4c75 🚀

--- static/js/965.54a78bed.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmedicine_delivery_app=self.webpackChunkmedicine_delivery_app||[]).push([[965],{965:function(e,n,r){r.r(n);var i=r(9439),t=r(2791),u=r(1572),a=r(2526),c=r(184);n.default=function(){var e=(0,t.useState)(a.Y),n=(0,i.Z)(e,2),r=n[0],p=n[1];return(0,c.jsx)(u.t.Group,{values:r,onReorder:p,children:r.map((function(e,n){return(0,c.jsx)(u.t.Item,{value:e,children:e.description},n)}))})}}}]);
-//# sourceMappingURL=965.54a78bed.chunk.js.map
